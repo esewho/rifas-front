c48f133573703b77138778c185e2892a99984e9e
esewho <albertogfcb@gmail.com>
2025-07-21 15:44:38 +0200

    Refactor RaffleDetail component: improve mobile and desktop layouts, enhance ticket selection logic, and integrate navigation functionality.

--- src/lib/raffle-organizer.ts
+++ src/lib/raffle-organizer.ts
@@ -1,17 +1,11 @@
-import httpService from "./http";
-import type { Raffle } from "../types/raffle";
+import httpService from "./http"
+import type { Raffle } from "../types/raffle"
 
-export async function createRaffleService(
-  raffle: FormData
-): Promise<Raffle> {
-  const response = await httpService.post<Raffle>(
-    "/raffle-organizer",
-    raffle,
-    {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    }
-  );
-  return response.data;
+export async function createRaffleService(raffle: FormData): Promise<Raffle> {
+	const response = await httpService.post<Raffle>("/raffle-organizer", raffle, {
+		headers: {
+			"Content-Type": "multipart/form-data",
+		},
+	})
+	return response.data
 }
